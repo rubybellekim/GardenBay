ec7430fe103e6772e672c571941af2175926b722
rubythebelle <rubybellekim@gmail.com>
2025-02-13 11:42:30 -0800

    Initial Commit

---
+++ GardenBay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GardenBay
+//
+//  Created by Ruby Kim on 2025-02-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GardenBay/GardenBayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenBayApp.swift
+//  GardenBay
+//
+//  Created by Ruby Kim on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct GardenBayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
